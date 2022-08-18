f63dd5129e4a723d0c56cda8159e2b7b80c033e0
chris_tamlyn <Chris_Tamlyn@next.co.uk>
2022-08-18 17:34:41 +0100

    Adds readme

--- fixtures/testFixture.ts
+++ fixtures/testFixture.ts
@@ -1,30 +1,18 @@
-// hello.ts
 import { test as base } from '@playwright/test';
-import { markTestStatus, instantiateBrowserstack } from '../utils/browserstack'
-import { capabilities }  from '../fixtures/browserstack_capabilities';
-import {Browser, Page} from '@playwright/test'
-// Extend base test with our fixtures.
+import { instantiateBrowserstack } from '../utils/browserstack'
+import { capabilities } from '../fixtures/browserstack_capabilities';
 
 type TestFixture = {
     testName: string;
-  };
+};
 
-//   export const test = base.extend<TestFixtures>( {
-//     testName : 'placeholder',
-//     async context( { testName, browser, page, context}, use ) {
-//         browser = await instantiateBrowserstack(capabilities, 'Example');
-//         page = await browser.newPage();  
-//       await use( page );
-//     },
-//   } );
-
-  export const test = base.extend<TestFixture>({
+export const test = base.extend<TestFixture>({
     testName: 'placeholder',
     page: async ({ page, browser, testName }, use) => {
         browser = await instantiateBrowserstack(capabilities, testName);
-        page = await browser.newPage();  
-      await use(page);
+        page = await browser.newPage();
+        await use(page);
     },
-  });
-// Now, this "test" can be used in multiple test files, and each of them will get the fixtures.
+});
+
 export default test;
---
+++ readME.md
@@ -0,0 +1,15 @@
+# Example of using Playwright with Browserstacks
+
+- Uses a fixture to extend the base test class, which automatically launches browserstacks for each test
+- Uses the page object model
+
+## Tp Run:
+
+- Setup a browserstack account
+- Create a API integration
+- Create a .env file at the root level
+- Populate like:
+```
+BROWSERSTACK_USERNAME = 'some_username'
+BROWSERSTACK_ACCESS_KEY = 'some_key'
+```
